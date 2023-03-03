505574d764bcae36df093503c557709c7765510a
qmpzzpmq <405691733@qq.com>
2023-03-03 22:36:57 +0800

    fit to 0.10.1 librosa

--- CLAAD/Transform/time_stretch.py
+++ CLAAD/Transform/time_stretch.py
@@ -10,8 +10,8 @@ from Transform.MelSpec import MelSpec
 
 
 def time_stretch(y, R_min=0.5, R_max=2, sr=16000):
-    R = np.random.uniform(R_min, R_max)
-    y_stretch = librosa.effects.time_stretch(y.detach().numpy(), R)
+    r = np.random.uniform(R_min, R_max)
+    y_stretch = librosa.effects.time_stretch(y.detach().numpy(), rate=r)
     y_stretch = librosa.resample(y_stretch, sr, sr * R)
     y_stretch = torch.from_numpy(y_stretch)
     S = MelSpec(y_stretch, sr)
